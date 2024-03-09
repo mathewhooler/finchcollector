3731a2b6d99ae1710dea2e734717aac29e0a56e1
mathewhooler <149305887+mathewhooler@users.noreply.github.com>
2024-03-09 18:11:09 +1100

    Finch Collector Lab - Finished

--- finchcollector/main_app/models.py
+++ finchcollector/main_app/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-
-# Create your models here.
+from django.urls import reverse
 
 class Finch(models.Model):
     name = models.CharField(max_length=100)
-    gender = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    
     def __str__(self):
-        return f'{self.name} is my name, and my gender is {self.gender}'
+        return f'{self.name} is my name, and my breed is {self.breed}'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
